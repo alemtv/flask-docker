13cb1e4d510a86872a8d08b482131cd9c6ff0dc3
Aleksandr <turiukompa@gmail.com>
2023-05-30 21:38:21 +0300

    added docker-compose and postgres image

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1>Hello from Flask & Docker</h1>'
+    return '<h1>Hello from Flask & Docker!!</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
